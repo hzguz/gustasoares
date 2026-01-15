5037ba236009d0ba5d28868d71957d78b4765a70
hzguz <gxdbeats@gmail.com>
2026-01-15 20:49:24 -0300

    chore: remove deprecated images.domains config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
-        domains: ['picsum.photos', 'images.unsplash.com'],
         remotePatterns: [
             {
                 protocol: 'https',
